267e90c3dd0b34460cff2700a4b3e6b3b3c0ed86
rmi7 <user@rmi7.me>
2018-06-03 22:37:19 +0200

    fix async/await usage in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -17,50 +17,52 @@ const CONTRACT_ADDRESSES = {
   },
 };
 
-module.exports = async (deployer, network) => {
-  // Migrations: gas 41915
+module.exports = (deployer, network) => {
+  deployer.then(async () => {
+    // Migrations: gas 41915
 
-  // gas: 4,643,520
-  await deployer.deploy(DetherCore, { gas: 6000000, gasPrice: 25000000000 });
+    // gas: 4,643,520
+    await deployer.deploy(DetherCore, { gas: 6000000, gasPrice: 25000000000 });
 
-  // gas 1,161,360
-  // await deployer.deploy(DetherToken, { gas: 5000000, gasPrice: 25000000000 });
+    // gas 1,161,360
+    // await deployer.deploy(DetherToken, { gas: 5000000, gasPrice: 25000000000 });
 
-  // gas 1,477,280
-  await deployer.deploy(DetherBank, { gas: 6000000, gasPrice: 25000000000 });
+    // gas 1,477,280
+    await deployer.deploy(DetherBank, { gas: 6000000, gasPrice: 25000000000 });
 
-  // gas 552,780
-  await deployer.deploy(SmsCertifier, { gas: 6000000, gasPrice: 25000000000 });
+    // gas 552,780
+    await deployer.deploy(SmsCertifier, { gas: 6000000, gasPrice: 25000000000 });
 
-  // gas 552,780
-  await deployer.deploy(KycCertifier, { gas: 6000000, gasPrice: 25000000000 });
+    // gas 552,780
+    await deployer.deploy(KycCertifier, { gas: 6000000, gasPrice: 25000000000 });
 
-  switch (network) {
-    case 'develop':
-      // use a fake instance to test locally using truffle develop
-    case 'development':
-      // use a fake instance to test locally using ganache
-      // fall through
-    case 'ropsten':
-      // Maker doesn't test on ropsten so we use a fake instance
-      await deployer.deploy(FakeExchangeRateOracle, { gas: 5000000, gasPrice: 25000000000 });
-      break;
+    switch (network) {
+      case 'develop':
+        // use a fake instance to test locally using truffle develop
+      case 'development':
+        // use a fake instance to test locally using ganache
+        // fall through
+      case 'ropsten':
+        // Maker doesn't test on ropsten so we use a fake instance
+        await deployer.deploy(FakeExchangeRateOracle, { gas: 5000000, gasPrice: 25000000000 });
+        break;
 
-    case 'kovan':
-      // fall through
+      case 'kovan':
+        // fall through
 
-    case 'mainnet':
-      await deployer.deploy(
-        ExchangeRateOracle,
-        // pass int he address of the Maker price feed contract on the blockchain
-        CONTRACT_ADDRESSES[network].mkrPriceFeed,
-        { gas: 5000000, gasPrice: 25000000000 },
-      );
-      break;
+      case 'mainnet':
+        await deployer.deploy(
+          ExchangeRateOracle,
+          // pass int he address of the Maker price feed contract on the blockchain
+          CONTRACT_ADDRESSES[network].mkrPriceFeed,
+          { gas: 6000000, gasPrice: 25000000000 },
+        );
+        break;
 
-    default:
-      throw new Error(`did not specify how to deploy ExchangeRateOracle on this network (${network})`);
-  }
+      default:
+        throw new Error(`did not specify how to deploy ExchangeRateOracle on this network (${network})`);
+    }
 
-  await deployer.deploy(DetherZoning, { gas: 6000000, gasPrice: 25000000000 });
+    await deployer.deploy(DetherZoning, { gas: 6000000, gasPrice: 25000000000 });
+  });
 };
