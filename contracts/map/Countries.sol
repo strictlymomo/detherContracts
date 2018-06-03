8c4f12609b64140974ea82a8f396a76b395fc137
rmi7 <user@rmi7.me>
2018-06-03 22:38:43 +0200

    update Countries.sol with bytes-to-array decoder function and other necessary changes

--- contracts/map/Countries.sol
+++ contracts/map/Countries.sol
@@ -7,17 +7,82 @@ contract Countries is Ownable {
   using SafeMath for uint256;
 
   // Matrix of countries (zoom level 12)
-  mapping (bytes2 => mapping(uint => uint[2][])) public countries;
+  mapping (bytes2 => mapping(uint => uint[])) public countries;
 
-  function updateCountry(bytes2 country, uint xIndex_, uint[2][] value)
+  function updateCountry(bytes2 country, uint xIndex_, uint[] values)
     public
     onlyOwner
   {
+    require(pairCount % 2 == 0);
+    uint pairCount = values.length / 2;
+    // we need an even amount of array values
+
     // TODO: require zoom level check
-    countries[country][xIndex_] = value;
+    countries[country][xIndex_] = values;
   }
 
-  function getYArraysLength(uint[2][] yArrays)
+  function toArray(bytes _bytes)
+    internal
+    returns (uint[][] _ptr)
+  {
+    assembly {
+      // Should probably do a modulo check to see if there is a zero remainder first
+      let total_bytes := div(mload(_bytes), 0x20)
+
+      _ptr := mload(0x40)
+      switch lt(_ptr, msize())
+      case 1 {
+        _ptr := msize()
+      }
+
+      // if no data goto end
+      jumpi(copy_end, eq(0, total_bytes))
+
+      let start_offset := add(_bytes, 0x20)
+      let num_arrays := mload(start_offset)
+      let array_offset := 0
+      let idx_arrays := 0
+      let num_elements := 0
+
+      // Find the total amount of elements
+      length_start:
+      jumpi(length_end, eq(idx_arrays, num_arrays))
+      array_offset := mload(add(start_offset, add(0x20, mul(idx_arrays,0x20))))
+      // Correct the pointers
+      mstore(add(start_offset, add(0x20, mul(idx_arrays,0x20))), add(_ptr, array_offset))
+      num_elements := add(num_elements, mload(array_offset))
+      idx_arrays := add(idx_arrays, 1)
+      jump(length_start)
+      length_end:
+
+      let idx_bytes := 0
+
+      // Copy all the data to the byte array
+      copy_start:
+      jumpi(copy_end, eq(total_bytes, idx_bytes))
+      mstore(add(_ptr, mul(idx_bytes, 0x20)), mload(add(start_offset, mul(idx_bytes, 0x20))))
+      idx_bytes := add(idx_bytes, 1)
+      jump(copy_start)
+      copy_end:
+
+      // Set the free memory pointer
+      mstore(0x40, add(_ptr, mul(0x20, total_bytes)))
+    }
+  }
+
+  // https://github.com/willitscale/learning-solidity/blob/master/support/NESTED_ARRAYS_NOT_IMPLEMENTED.MD#32-convert-bytes-to-a-nested-array
+  function updateCountryBatch(bytes2 country, uint[] xIndexBatch_, bytes valueBatch)
+    public
+    onlyOwner
+  {
+    uint[][] memory parsed = toArray(valueBatch);
+
+    for (uint i = 0; i < xIndexBatch_.length; i++) {
+      updateCountry(country, xIndexBatch_[i], parsed[i]);
+    }
+  }
+
+  function getYArraysLength(uint[] yArrays)
     public
     pure
     returns (uint yCount_)
@@ -28,7 +93,7 @@ contract Countries is Ownable {
   function getCountryColumn(uint xIndex_, bytes2 country)
     public
     view
-    returns (uint[2][] column_)
+    returns (uint[] column_)
   {
     column_ = countries[country][xIndex_];
   }
@@ -51,24 +116,19 @@ contract Countries is Ownable {
     uint y;
     (x, y) = getParentTile18to12(x18, y18);
 
-    uint[2][] memory yArrays = getCountryColumn(x, country);
+    uint[] memory yArrays = getCountryColumn(x, country);
+    uint yArraysLen = getYArraysLength(yArrays);
 
-    if (getYArraysLength(yArrays) ==  0) {
-      insideCountry_ = false;
-    } else {
-      for (uint i = 0; i < getYArraysLength(yArrays) ; i++){
-        uint[2] memory yArray = yArrays[i];
-        uint yLeft = yArray[0];
-        uint yRight = yArray[1];
+    insideCountry_ = false;
+    if (yArraysLen >  0) {
+      for (uint i = 0; i < yArraysLen / 2; i += 2){
+        uint yLeft = yArrays[i];
+        uint yRight = yArrays[i+1];
 
         if (y >= yLeft && y <= yRight) {
           insideCountry_ = true;
           break;
         }
-      }
-
-      if (insideCountry_ != true) {
-        insideCountry_ = false;
       }
     }
   }
